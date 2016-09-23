a13befe08f2d4bab38518e3c2d011f87c6b33b08
Alex Diaz <ealejandro@outlook.com>
2016-09-23 10:47:29 -0400

    Woops, templates go in header files...

--- src/include/string.h
+++ src/include/string.h
@@ -8,17 +8,87 @@ namespace ea
     class basic_string
     {
     public:
-        basic_string();
+        basic_string()
+            :
+            data_(new T[START_CAPACITY]),
+            length_(0),
+            capacity_(START_CAPACITY)
+        {
+
+        }
         basic_string(const char* str);
-        basic_string(const basic_string& other);
-        basic_string& opertor=(const basic_string& rhs);
-        std::vector<basic_string> find(const basic_string& pattern) const;  
-        template<typename T>
+        {
+            length_ = std::strlen(ptr);
+            capacity_ = length_*2 + 1;
+            data_ = new T[capacity_];
+            std::strcpy(data_, ptr);
+        }
+        basic_string(const basic_string& other)
+        {
+            length_ = other.length_;
+            capacity_ = other.capacity_;
+            data_ = new T[capacity_];
+            std::memcpy(data_, other.data_, length_*sizeof(T));
+        }
+        basic_string<T>& operator=(const basic_string& rhs)
+        {
+            basic_string tmp_str = rhs;
+            swap(*this, rhs);
+        }
+        const T* find_first_not_of(T character) const
+        {
+            auto current = begin();
+            while(*current != character) {
+                ++current;
+            }
+            return current;
+        }
+        const T* find_last_not_of(T character) const
+        {
+            auto current = end()-1;
+            while(*current != character) {
+                ++current;
+            }
+            return current;
+        }
         const T* begin() const
-        const T* end() const;
-        void trim();
-        void crop(basic_string::iterator& begin, basic_string::iterator end);
-        ~basic_string();
+        {
+            return &data_[0];
+        }
+        const T* end() const
+        {
+            return &data_[length+1];
+        }
+        void trim()
+        {
+            auto start = find_first_not_of(' ');
+            auto finish = find_last_not_of(' ');
+            length_ = finish - start;
+            for(int i = 0; start != finish + 1; ++start) {
+                data_[i] = *start;
+            }
+        }
+        void crop(T* begin, T* end)
+        {
+            int i = 0;
+            length_ = end - begin;
+            while(begin != end) {
+                data_[i] = *begin;
+                ++begin;
+            }
+            data_[i] = *begin;
+        }
+        std::vector<string<T>> split(T character)
+        {
+            std::vector<string<T>> findings;
+            //if(char in data_ == character)
+            //findgings += character;
+            //....
+        }
+        ~basic_string()
+        {
+
+        }
     private:
         std::shared_ptr<data_> data_;
         std::size_t length_;
